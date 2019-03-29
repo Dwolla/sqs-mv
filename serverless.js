65c39c15ee5ec3e77f15754e3d4b22b9ff8cecc3
Rocky Warren <1085683+therockstorm@users.noreply.github.com>
2019-03-29 17:04:11 -0500

    grant permissions, increase timeout

--- serverless.js
+++ serverless.js
@@ -2,5 +2,34 @@ const { serverless } = require("skripts/config")
 
 module.exports = {
   ...serverless,
-  functions: { func: { handler: "src/handler.handle" } }
+  custom: {
+    ...serverless.custom,
+    tags: {
+      ...serverless.custom.tags,
+      Team: "growth",
+      Visibility: "external",
+      DeployJobUrl: "${env:BUILD_URL, 'n/a'}",
+      "org.label-schema.vcs-url": "${env:GIT_URL, 'n/a'}",
+      "org.label-schema.vcs-ref": "${env:GIT_COMMIT, 'n/a'}"
+    }
+  },
+  plugins: [...serverless.plugins, "serverless-pseudo-parameters"],
+  provider: {
+    ...serverless.provider,
+    iamRoleStatements: [
+      {
+        Effect: "Allow",
+        Action: ["sqs:DeleteMessage", "sqs:ReceiveMessage"],
+        Resource:
+          "arn:aws:sqs:${self:provider.region}:#{AWS::AccountId}:webhooks-error-${self:provider.stage}"
+      },
+      {
+        Effect: "Allow",
+        Action: ["sqs:SendMessage"],
+        Resource:
+          "arn:aws:sqs:${self:provider.region}:#{AWS::AccountId}:webhooks-*-consumer-queue-${self:provider.stage}"
+      }
+    ]
+  },
+  functions: { func: { handler: "src/handler.handle", timeout: 30 } }
 }
